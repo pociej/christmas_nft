d15455e6a7fc8c8c3d205c6305adc8a72e67e909
pociej <grzegorz.pociejewski@gmail.com>
2023-12-22 00:50:52 +0100

    dupa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { Dispatch, SetStateAction, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import "./dots.css";
 import { Message, useConversation } from "./chat";
